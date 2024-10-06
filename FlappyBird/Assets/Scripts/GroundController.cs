2edb259047d4a7c030886c9b2d07cb9c8032c47c
Samu163 <125267861+Samu163@users.noreply.github.com>
2024-10-06 12:42:21 +0200

    Scene And Ui

--- FlappyBird/Assets/Scripts/GroundController.cs
+++ FlappyBird/Assets/Scripts/GroundController.cs
@@ -5,27 +5,27 @@ using UnityEngine;
 public class GroundController : MonoBehaviour
 {
 
-    public int size = 0;
-    public int velocity = 0;
-    public Rigidbody2D rigidbody2D;
-    public SpriteRenderer spriteRenderer;
+    public float velocity = 1f;
+    public float size = 10f;
+        
+    SpriteRenderer _spriteRenderer;
 
     private Vector2 _startSize;
 
     // Start is called before the first frame update
     void Start()
     {
-        //spriteRenderer = GetComponent<Rigidbody2D>();
-        _startSize = spriteRenderer.bounds.size;
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _startSize = new Vector2(_spriteRenderer.size.x, _spriteRenderer.size.y);
     }
 
     // Update is called once per frame
     void Update()
     {
-        spriteRenderer.size = new Vector2(spriteRenderer.size.x + velocity * Time.deltaTime, spriteRenderer.size.y);
-        if (spriteRenderer.bounds.size.x > size)
+        _spriteRenderer.size = new Vector2(_spriteRenderer.size.x + velocity * Time.deltaTime, _spriteRenderer.size.y);
+        if (_spriteRenderer.size.x > size)
         {
-            spriteRenderer.size = _startSize;
+            _spriteRenderer.size = _startSize;
         }
     }
 }
