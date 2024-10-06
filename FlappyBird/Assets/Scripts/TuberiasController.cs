bac32eed13b7c122dd721e111c67019b3e3aa8b1
D1dii <didacgp123@gmail.com>
2024-10-06 12:43:18 +0200

    Scriptsa

---
+++ FlappyBird/Assets/Scripts/TuberiasController.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TuberiasController : MonoBehaviour
+{
+
+    public float speed;
+    public Rigidbody2D tuberiaRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tuberiaRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tuberiaRigidbody.velocity = Vector2.left * speed;
+
+        if(transform.position.x < -6)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ FlappyBird/Assets/Scripts/TuberiasInstantiator.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TuberiasInstantiator : MonoBehaviour
+{
+
+    public GameObject tuberiasPrefab;
+    public float positionX;
+    public float positionY;
+    public float timer;
+    public float instantiateCooldown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > instantiateCooldown)
+        {
+            timer = 0;
+            positionY = Random.Range(-2.5f, 7.3f);
+            Instantiate(tuberiasPrefab);
+            tuberiasPrefab.transform.position = new Vector3(positionX, positionY, tuberiasPrefab.transform.position.z);
+        }
+    }
+}
