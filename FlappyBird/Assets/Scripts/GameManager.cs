8200e72b42526aed161adc00491d14f50d3fcd0f
D1dii <didacgp123@gmail.com>
2024-10-06 12:56:46 +0200

    s

---
+++ FlappyBird/Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    public int highScore = 0;
+    public int score = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ FlappyBird/Assets/Scripts/UiController.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UiController : MonoBehaviour
+{
+    public TextMeshPro highScore;
+    public TextMeshPro score;
+
+
+
+    public void Update()
+    {
+        
+    }
+}
